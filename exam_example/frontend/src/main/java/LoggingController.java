aa5586937d3a30705e3fe3d3e08d09192098f895
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-11 15:40:59 +0200

    Created LoggingController

--- exam_example/frontend/src/main/java/LoggingController.java
+++ exam_example/frontend/src/main/java/LoggingController.java
@@ -1,5 +1,131 @@
+import com.iliani14.pg5100.exam_example.Countries;
+import com.iliani14.pg5100.exam_example.ejb.UserEJB;
+import com.iliani14.pg5100.exam_example.entity.User;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by anitailieva on 10/10/2016.
  */
-public class LoggingController {
+
+@Named
+@SessionScoped
+public class LoggingController implements Serializable {
+
+
+    @EJB
+    private UserEJB userEJB;
+
+
+    private String formUserName;
+    private String formPassword;
+    private String formConfirmPassword;
+    private String formFirstName;
+    private String formMiddleName;
+    private String formLastName;
+    private String formCountry;
+
+    private String registeredUser;
+
+
+    public LoggingController(){
+
+    }
+    public String login(String userId, String password) {
+        boolean valid = userEJB.login(userId, password);
+        if (valid) {
+            registeredUser = formUserName;
+            return "home.jsf";
+        } else {
+            return "login.jsf";
+        }
+    }
+
+    public String logOut(){
+    registeredUser = null;
+        return "home.jsf";
+    }
+
+    public String getRegisteredUser(){
+        return registeredUser;
+    }
+
+    public List<String> getUserCountry() {
+    return Countries.getCountries();
+    }
+    public String getCountry() {
+        if (registeredUser == null) {
+            return null;
+        }
+        return userEJB.getUser(registeredUser).getCountry();
+    }
+
+    public boolean loggedIn() {
+        return registeredUser != null;
+    }
+    public User getUser() {
+    return userEJB.getUser(registeredUser);
+    }
+
+    public String getFormUserName() {
+        return formUserName;
+    }
+
+    public void setFormUserName(String formUserName) {
+        this.formUserName = formUserName;
+    }
+
+    public String getFormPassword() {
+        return formPassword;
+    }
+
+    public void setFormPassword(String formPassword) {
+        this.formPassword = formPassword;
+    }
+
+    public String getFormConfirmPassword() {
+        return formConfirmPassword;
+    }
+
+    public void setFormConfirmPassword(String formConfirmPassword) {
+        this.formConfirmPassword = formConfirmPassword;
+    }
+
+    public String getFormFirstName() {
+        return formFirstName;
+    }
+
+    public void setFormFirstName(String formFirstName) {
+        this.formFirstName = formFirstName;
+    }
+
+    public String getFormMiddleName() {
+        return formMiddleName;
+    }
+
+    public void setFormMiddleName(String formMiddleName) {
+        this.formMiddleName = formMiddleName;
+    }
+
+    public String getFormLastName() {
+        return formLastName;
+    }
+
+    public void setFormLastName(String formLastName) {
+        this.formLastName = formLastName;
+    }
+
+    public String getFormCountry() {
+        return formCountry;
+    }
+
+    public void setFormCountry(String formCountry) {
+        this.formCountry = formCountry;
+    }
 }
+
+
