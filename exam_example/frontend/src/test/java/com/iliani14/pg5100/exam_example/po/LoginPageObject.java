ef409aa7d21de7ae95e3bd0cec7494f1fd11014f
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-12 22:31:36 +0200

    Created LoginPageObject which so far contains doLogin and doCancel methods

---
+++ exam_example/frontend/src/test/java/com/iliani14/pg5100/exam_example/po/LoginPageObject.java
@@ -0,0 +1,45 @@
+package com.iliani14.pg5100.exam_example.po;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by anitailieva on 12/10/2016.
+ */
+public class LoginPageObject extends PageObject{
+
+    public LoginPageObject(WebDriver driver) {
+        super(driver);
+
+        assertEquals("Login", driver.getTitle());
+        assertFalse(isLoggedIn());
+
+    }
+
+    @Override
+    public boolean isOnPage() {
+        return getDriver().getTitle().equals("Login");
+    }
+
+    public HomePageObject doLogin(String user, String password){
+
+        setText("loginForm:userName", user);
+        setText("loginForm:password", password);
+        getDriver().findElement(By.id("loginForm:loginButton"));
+
+        if(isOnPage()){
+            return null;
+        }else {
+            return new HomePageObject(getDriver());
+        }
+    }
+
+    public HomePageObject doCancel(){
+        getDriver().findElement(By.id("cancel")).click();
+        waitForPageToLoad();
+        return new HomePageObject(getDriver());
+    }
+}
