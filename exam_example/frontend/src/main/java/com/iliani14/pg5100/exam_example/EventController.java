01ace0d959ed2c1d1068670f71c524e8e730e378
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-15 21:49:04 +0200

    Created controller which contains a method called creatEvent

--- exam_example/frontend/src/main/java/com/iliani14/pg5100/exam_example/EventController.java
+++ exam_example/frontend/src/main/java/com/iliani14/pg5100/exam_example/EventController.java
@@ -1,10 +1,15 @@
+package com.iliani14.pg5100.exam_example;
+
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 
 /**
  * Created by anitailieva on 10/10/2016.
  */
 @Named
 @SessionScoped
-public class EventController {
+public class EventController implements Serializable {
+
+
 }
