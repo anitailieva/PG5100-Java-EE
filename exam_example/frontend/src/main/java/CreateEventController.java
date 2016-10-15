af75fa646b9543187f7daab79fed901e3270bede
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-15 21:48:06 +0200

    Created page for creating new event

--- exam_example/frontend/src/main/java/CreateEventController.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by anitailieva on 10/10/2016.
- */
-public class CreateEventController {
-}
---
+++ exam_example/frontend/src/main/java/com/iliani14/pg5100/exam_example/EventController.java
@@ -0,0 +1,10 @@
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * Created by anitailieva on 10/10/2016.
+ */
+@Named
+@SessionScoped
+public class EventController {
+}
