6ba7d797ed0e8be7a3d2e8cae3fa595e591f2d53
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-15 13:32:50 +0200

    Created selenium tests for login, creating a valid user and password mismatch

--- exam_example/frontend/src/test/java/com/iliani14/pg5100/exam_example/WebPageIT.java
+++ exam_example/frontend/src/test/java/com/iliani14/pg5100/exam_example/WebPageIT.java
@@ -1,50 +1,87 @@
 package com.iliani14.pg5100.exam_example;
 
+import com.iliani14.pg5100.exam_example.po.CreateUserPageObject;
 import com.iliani14.pg5100.exam_example.po.HomePageObject;
 import com.iliani14.pg5100.exam_example.po.LoginPageObject;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by anitailieva on 11/10/2016.
  */
 
 public class WebPageIT extends WebTestBase {
-    private HomePageObject hp;
 
     @Before
     public void toStartingPage() {
-        hp = new HomePageObject(getDriver());
-        hp.toStartingPage();
-        hp.logout();
-        assertTrue(hp.isOnPage());
-        assertFalse(hp.isLoggedIn());
+        hpo = new HomePageObject(getDriver());
+        hpo.toStartingPage();
+        hpo.logout();
+        assertTrue(hpo.isOnPage());
+        assertFalse(hpo.isLoggedIn());
 
     }
 
     @Test
     public void testHomePage() {
-        hp.toStartingPage();
-        assertTrue(hp.isOnPage());
+        hpo.toStartingPage();
+        assertTrue(hpo.isOnPage());
 
     }
 
     @Test
     public void testLoginLink() {
-        LoginPageObject loginPageObject = hp.goToLogin();
+        LoginPageObject loginPageObject = hpo.goToLogin();
         assertTrue(loginPageObject.isOnPage());
     }
 
     @Test
     public void testLoginWrongUser(){
-        LoginPageObject lo = hp.goToLogin();
+        LoginPageObject lo = hpo.goToLogin();
         HomePageObject ho =  lo.doLogin("name", "Pass");
         assertNull(ho);
         assertTrue(lo.isOnPage());
     }
 
+    @Test
+    public void testCreateUserFailDueToPasswordMismatch(){
+        LoginPageObject login = hpo.goToLogin();
+        CreateUserPageObject create = login.createNewUser();
+
+        HomePageObject home = create.createUser("user", "pass", "password", "name", "surname", "Bulgaria");
+        assertNull(home);
+        assertTrue(create.isOnPage());
+
+    }
+
+    @Test
+    public void testCreateValidUser(){
+        LoginPageObject loginPageObject = hpo.goToLogin();
+        CreateUserPageObject user = loginPageObject.createNewUser();
+        assertTrue(user.isOnPage());
+
+        String userId = "user";
+        HomePageObject home = user.createUser(userId, "password", "password", "name", "surname", "United States");
+        assertNotNull(home);
+        assertTrue(home.isOnPage());
+        assertTrue(home.isLoggedIn(userId));
+
+        home.logout();
+        assertFalse(home.isLoggedIn());
+    }
+
+    @Test
+    public void testLogin(){
+        createAndLogNewUser("userId", "name", "surname", "Norway");
+        hpo.logout();
+        assertFalse(hpo.isLoggedIn());
+
+        LoginPageObject login = hpo.goToLogin();
+        HomePageObject home = login.doLogin("userId", "pass");
+        assertNotNull(home);
+        assertTrue(home.isOnPage());
+        assertTrue(home.isLoggedIn());
+    }
 }
